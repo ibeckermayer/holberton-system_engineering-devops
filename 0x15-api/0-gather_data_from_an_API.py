313cc75f3324f7b2c81a69611940ca8589a49fc7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-29 04:21:40 +0000

    0 and setup

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+gathers information about an employee by ID and returns their TODO progress
+'''
+import requests
+import sys
+
+
+def info():
+    raw_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(sys.argv[1]))
+    names = raw_users.json().get('name')
+    raw_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    comp = 0
+    titles = []
+    tot = 0
+    for todo in raw_todos:
+        if todo['userId'] == int(sys.argv[1]):
+            if todo['completed'] is True:
+                comp += 1
+                titles.append(todo['title'])
+                tot += 1
+                print("Employee {} is done with raw_todos({}/{}):"
+                      .format(names, comp, tot))
+    for title in titles:
+        print('\t ', end="")
+        print(title)
+
+
+if __name__ == "__main__":
+    info()
---
+++ 0x15-api/README.md
@@ -0,0 +1,17 @@
+# 0x15. API
+## 0. Gather data from an API
+***
+Write a Python script that, using this 
+
+## 1. Export to CSV
+***
+, for a given employee ID, returns information about his/her TODO list progress.
+
+## 2. Export to JSON
+***
+Using what you did in the task #0, extend your Python script to export data in the CSV format.
+
+## 3. Dictionary of list of dictionaries
+***
+Using what you did in the task #0, extend your Python script to export data in the JSON format.
+
