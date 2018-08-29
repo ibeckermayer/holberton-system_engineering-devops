73c730bce6931446349a946baa436ffbf0dd0c66
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-29 04:47:09 +0000

    changed tab to space. wtf

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,25 @@ import sys
 
 
 def info():
-
-    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                       .format(sys.argv[1]))
-    name = emp.json().get('name')
-    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
-    tasks = tasks.json()
-    complete = 0
+    raw_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(sys.argv[1]))
+    names = raw_users.json().get('name')
+    raw_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos')
+    raw_todos = raw_todos.json()
+    comp = 0
     titles = []
-    total = 0
-    for task in tasks:
-        if task['userId'] == int(sys.argv[1]):
-            if task['completed'] is True:
-                complete += 1
-                titles.append(task['title'])
-            total += 1
+    tot = 0
+    for todo in raw_todos:
+        if todo['userId'] == int(sys.argv[1]):
+            if todo['completed'] is True:
+                comp += 1
+                titles.append(todo['title'])
+            tot += 1
     print("Employee {} is done with tasks({}/{}):"
-          .format(name, complete, total))
+          .format(names, comp, tot))
     for title in titles:
-        print('\t ', end="")
+        print('     ', end="")
         print(title)
 
 
