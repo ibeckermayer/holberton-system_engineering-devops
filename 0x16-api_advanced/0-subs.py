ce67e3d8ae4bcd7c2554fe8d1edb58c1ff113527
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-30 22:31:16 +0000

    0 and init

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from requests import get
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """get number of subscribers for a subreddit"""
+    header = {'User-Agent': 'Isaiah Becker-Mayer'}
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=header).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
+        return 0
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
